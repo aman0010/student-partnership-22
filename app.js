ff93231f5f9d33f43f38bd0de27fc0072246d6d4
aman0010 <shresthaaman416@gmail.com>
2022-04-26 11:30:59 +0545

    added endpoint to get user ID

--- app.js
+++ app.js
@@ -6,11 +6,11 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 
+const token = process.env.token
+const config = {
+    headers: { Authorization: `Bearer ${token}` },
+};
 app.get("/", function (req, res) {
-    const token = process.env.token
-    const config = {
-        headers: { Authorization: `Bearer ${token}` },
-    };
     axios
         .get(
             "https://api.twitter.com/2/users/1508492625711878145/tweets?start_time=2022-03-25T00:00:01Z&max_results=100",
@@ -20,6 +20,18 @@ app.get("/", function (req, res) {
             res.send(resp.data);
         });
 });
+
+app.get("/user/:username", function(req, res) {
+    const username = req.params.username
+    axios
+        .get(
+            `https://api.twitter.com/2/users/by/username/${username}`,
+            config
+        )
+        .then((resp) => {
+            res.send(resp.data);
+        });
+})
 
 app.listen(process.env.port, function () {
     console.log(`App listening on port ${process.env.port}!`);
